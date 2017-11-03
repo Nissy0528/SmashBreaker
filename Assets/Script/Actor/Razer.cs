7245ce7dcd10dc29888890b91683ee37b5030b97
Nissy0528 <hornetsouki@gmail.com>
2017-11-03 17:27:22 +0900

    ビームの予備動作追加

--- Assets/Script/Actor/Razer.cs
+++ Assets/Script/Actor/Razer.cs
@@ -4,124 +4,159 @@ using UnityEngine;
 
 public class Razer
 {
-	private const float limit = 100f;
+    private const float limit = 100f;
 
-	/// <summary>
-	/// 原点の空オブジェクト
-	/// </summary>
-	private GameObject origin;
+    /// <summary>
+    /// 原点の空オブジェクト
+    /// </summary>
+    private GameObject origin;
 
-	/// <summary>
-	/// 速度
-	/// </summary>
-	private float speed;
-	/// <summary>
-	/// 距離
-	/// </summary>
-	[SerializeField]
-	private float distance = 0.0f;
-	public Ray2D shotRay;
+    /// <summary>
+    /// 速度
+    /// </summary>
+    private float speed;
+    /// <summary>
+    /// 距離
+    /// </summary>
+    [SerializeField]
+    private float distance = 0.0f;
+    public Ray2D shotRay;
 
-	/// <summary>
-	/// 線の描画
-	/// </summary>
-	private LineRenderer lineRenderer;
-	private RaycastHit2D shotHit;
-	private int layerMask;
+    /// <summary>
+    /// 線の描画
+    /// </summary>
+    private LineRenderer lineRenderer;
+    private RaycastHit2D shotHit;
+    private int layerMask;
 
-	/// <summary>
-	/// 遮蔽物レイヤー
-	/// </summary>
-	private string shieldLayer;
+    private float time;
 
-	public Razer(Transform shooter, Vector3 velocity, float speed, string shieldLayer, params string[] targetLayers)
-	{
-		//空オブジェクトの生成
-		origin = new GameObject("shooter");
-		var transform = origin.transform;
-		transform.SetParent(shooter);
-		transform.localPosition = Vector3.zero;
+    /// <summary>
+    /// 遮蔽物レイヤー
+    /// </summary>
+    private string shieldLayer;
 
-		this.speed = speed;
+    public Razer(Transform shooter, Vector3 velocity, float speed, string shieldLayer, Material mat, params string[] targetLayers)
+    {
+        //空オブジェクトの生成
+        origin = new GameObject("shooter");
+        var transform = origin.transform;
+        transform.SetParent(shooter);
+        transform.localPosition = Vector3.zero;
 
-		///線の作成
-		lineRenderer = origin.AddComponent<LineRenderer>();
-		//当たり判定付けたい分だけ追加
-		layerMask = LayerMask.GetMask(targetLayers);
-		//ビームの表示
-		lineRenderer.enabled = true;
-		//原点の設定
-		lineRenderer.SetPosition(0, transform.position);
-		//レイの設定
-		shotRay.origin = transform.position;
-		shotRay.direction = velocity.normalized;
+        this.speed = speed;
 
-		this.shieldLayer = shieldLayer;
-	}
+        ///線の作成
+        lineRenderer = origin.AddComponent<LineRenderer>();
+        //当たり判定付けたい分だけ追加
+        layerMask = LayerMask.GetMask(targetLayers);
+        //ビームの表示
+        lineRenderer.enabled = true;
+        //原点の設定
+        lineRenderer.SetPosition(0, transform.position);
+        //レイの設定
+        shotRay.origin = transform.position;
+        shotRay.direction = velocity.normalized;
+        //マテリアル設定
+        lineRenderer.material = mat;
+        Color color = lineRenderer.material.color;
+        color.a = 0.0f;
+        lineRenderer.material.color = color;
+        //幅設定
+        lineRenderer.startWidth = 0.0f;
+        lineRenderer.endWidth = 0.0f;
 
-	/// <summary>
-	/// 更新
-	/// </summary>
-	public void Update()
-	{
-		LineUpdate();
+        this.shieldLayer = shieldLayer;
+    }
 
-	}
+    /// <summary>
+    /// 更新
+    /// </summary>
+    public void Update()
+    {
+        LineUpdate();
 
-	private void LineUpdate()
-	{
+    }
 
-		//長さの加算
-		distance += speed * Time.deltaTime;
+    private void LineUpdate()
+    {
 
-		distance = Mathf.Min(distance, limit);
-		
+        //長さの加算
+        distance += speed * Time.deltaTime;
 
-		ShieldCheck();
-		Vector3 kz = shotRay.origin + (Vector2)(origin.transform.rotation * shotRay.direction) * distance;
-
-
-		lineRenderer.SetPosition(0, origin.transform.position);
-		lineRenderer.SetPosition(1, kz);
-	}
-
-	private void ShieldCheck()
-	{
-		Vector2 rotdir = (origin.transform.rotation * shotRay.direction);
-		Vector3 kz = shotRay.origin + rotdir * distance;
-		//当たったobj取得
-		float st = Vector2.Distance(origin.transform.position, kz);
-		shotHit = Physics2D.Raycast(origin.transform.position, rotdir, st, LayerMask.GetMask(shieldLayer));
-		if (shotHit)
-		{
-			distance = Vector2.Distance(origin.transform.position, shotHit.transform.position);
-		}
+        distance = Mathf.Min(distance, limit);
 
 
-	}
+        ShieldCheck();
+        Vector3 kz = shotRay.origin + (Vector2)(origin.transform.rotation * shotRay.direction) * distance;
 
-	/// <summary>
-	/// 当たり判定
-	/// </summary>
-	/// <returns></returns>
-	public Collider2D GetHit()
-	{
-		Vector2 rotdir = (origin.transform.rotation * shotRay.direction);
-		Vector3 kz = shotRay.origin + rotdir * distance;
-		//当たったobj取得
-		float st = Vector2.Distance(origin.transform.position, kz);
-		shotHit = Physics2D.Raycast(origin.transform.position, rotdir, st, layerMask);
 
-		return shotHit.collider;
-	}
+        lineRenderer.SetPosition(0, origin.transform.position);
+        lineRenderer.SetPosition(1, kz);
 
-	/// <summary>
-	/// 停止
-	/// </summary>
-	public void Stop()
-	{
-		lineRenderer.enabled = false;
-		distance = 0;
-	}
+        time += Time.deltaTime;
+
+        lineRenderer.startWidth = Mathf.Min(lineRenderer.startWidth + Time.deltaTime, 0.25f);
+        lineRenderer.endWidth = Mathf.Min(lineRenderer.endWidth + Time.deltaTime, 0.25f);
+
+        if (time >= 2.5f)
+        {
+            lineRenderer.startWidth = 1.0f;
+            lineRenderer.endWidth = 1.0f;
+            Color color = lineRenderer.material.color;
+            color.a = 1.0f;
+            lineRenderer.material.color = color;
+        }
+
+    }
+
+    private void ShieldCheck()
+    {
+        Vector2 rotdir = (origin.transform.rotation * shotRay.direction);
+        Vector3 kz = shotRay.origin + rotdir * distance;
+        //当たったobj取得
+        float st = Vector2.Distance(origin.transform.position, kz);
+        shotHit = Physics2D.Raycast(origin.transform.position, rotdir, st, LayerMask.GetMask(shieldLayer));
+        if (shotHit)
+        {
+            distance = Vector2.Distance(origin.transform.position, shotHit.transform.position);
+        }
+
+
+    }
+
+    /// <summary>
+    /// 当たり判定
+    /// </summary>
+    /// <returns></returns>
+    public Collider2D GetHit()
+    {
+        if (time < 2.5f || lineRenderer.enabled == false) return null;
+
+        Vector2 rotdir = (origin.transform.rotation * shotRay.direction);
+        Vector3 kz = shotRay.origin + rotdir * distance;
+        //当たったobj取得
+        float st = Vector2.Distance(origin.transform.position, kz);
+        shotHit = Physics2D.Raycast(origin.transform.position, rotdir, st, layerMask);
+
+        return shotHit.collider;
+    }
+
+    /// <summary>
+    /// 停止
+    /// </summary>
+    public void Stop(bool isEnabled)
+    {
+        lineRenderer.enabled = isEnabled;
+        distance = 0;
+        //マテリアル設定
+        Color color = lineRenderer.material.color;
+        color.a = 0.0f;
+        lineRenderer.material.color = color;
+        //幅設定
+        lineRenderer.startWidth = 0.0f;
+        lineRenderer.endWidth = 0.0f;
+        time = 0.0f;
+    }
 }
 
--- Assets/Script/Actor/RazerShooter.cs
+++ Assets/Script/Actor/RazerShooter.cs
@@ -7,111 +7,137 @@ using UnityEngine;
 /// </summary>
 public class RazerShooter : MonoBehaviour
 {
-	/// <summary>
-	/// 停止フラグ
-	/// </summary>
-	private bool isStop;
+    /// <summary>
+    /// 停止フラグ
+    /// </summary>
+    private bool isStop;
 
-	/// <summary>
-	/// 爆破
-	/// </summary>
-	[SerializeField]
-	private GameObject particle;
+    /// <summary>
+    /// 爆破
+    /// </summary>
+    [SerializeField]
+    private GameObject particle;
 
-	/// <summary>
-	/// 速度
-	/// </summary>
-	[SerializeField]
-	private float speed = 10f;
+    /// <summary>
+    /// 速度
+    /// </summary>
+    [SerializeField]
+    private float speed = 10f;
 
-	/// <summary>
-	/// レーザー
-	/// </summary>
-	private List<Razer> razerList;
+    /// <summary>
+    /// レーザー
+    /// </summary>
+    private List<Razer> razerList;
 
-	/// <summary>
-	/// 飛ばす方向
-	/// </summary>
-	[SerializeField]
-	private Vector3[] velocitys;
+    /// <summary>
+    /// 飛ばす方向
+    /// </summary>
+    [SerializeField]
+    private Vector3[] velocitys;
 
-	/// <summary>
-	/// 標的のレイヤー
-	/// </summary>
-	[SerializeField]
-	private string[] targetLayers;
-	/// <summary>
-	/// 遮蔽物のレイヤー
-	/// </summary>
-	[SerializeField]
-	private string shieldLayer;
+    /// <summary>
+    /// 標的のレイヤー
+    /// </summary>
+    [SerializeField]
+    private string[] targetLayers;
+    /// <summary>
+    /// 遮蔽物のレイヤー
+    /// </summary>
+    [SerializeField]
+    private string shieldLayer;
 
-	public void Start()
-	{
-		Init();
-	}
+    public Material mat;
+    public float razerTime;
 
-	/// <summary>
-	/// 初期化
-	/// </summary>
-	public void Init()
-	{
-		razerList = new List<Razer>();
-		for (int i = 0; i < velocitys.Length; i++)
-		{
-			razerList.Add(new Razer(transform, velocitys[i], speed, shieldLayer,targetLayers));
-		}
-	}
+    private float razerCount;
+    private bool isEnable;
 
-	/// <summary>
-	/// 更新
-	/// </summary>
-	public void Update()
-	{
-		foreach (var r in razerList)
-		{
-			r.Update();
-			HitCheck(r);
-		}
-	}
+    public void Start()
+    {
+        Init();
+    }
 
-	/// <summary>
-	/// 当たり判定
-	/// </summary>
-	/// <param name="r"></param>
-	private void HitCheck(Razer r)
-	{
-		var rCol = r.GetHit();
+    /// <summary>
+    /// 初期化
+    /// </summary>
+    public void Init()
+    {
+        razerList = new List<Razer>();
+        for (int i = 0; i < velocitys.Length; i++)
+        {
+            razerList.Add(new Razer(transform, velocitys[i], speed, shieldLayer, mat, targetLayers));
+        }
+        razerCount = 0.0f;
+        isEnable = true;
+    }
 
-		if (rCol != null)
-		{
-			var posi = rCol.transform.position;
-			//与ダメージ処理
-			AddDamage(rCol.gameObject);
-			//撃破パーティクル
-			//var p = Instantiate(particle);
-			//p.transform.position = posi;
-			//Destroy(p, 5f);
-		}
-	}
+    /// <summary>
+    /// 更新
+    /// </summary>
+    public void Update()
+    {
+        foreach (var r in razerList)
+        {
+            r.Update();
+            HitCheck(r);
+        }
+        Swich();
+    }
 
-	private void AddDamage(GameObject col)
-	{
-		if(col.gameObject.tag == "Player")
-		{
-			//col.GetComponent<Player>().Damage();
-		}
-	}
+    /// <summary>
+    /// ビーム切り替え
+    /// </summary>
+    private void Swich()
+    {
+        if(razerCount>0.0f)
+        {
+            razerCount -= Time.deltaTime;
+            return;
+        }
 
-	/// <summary>
-	/// 停止
-	/// </summary>
-	public void Stop()
-	{
-		isStop = true;
-		foreach (var r in razerList)
-		{
-			r.Stop();
-		}
-	}
+        isEnable = !isEnable;
+        Stop();
+        razerCount = razerTime;
+    }
+
+    /// <summary>
+    /// 当たり判定
+    /// </summary>
+    /// <param name="r"></param>
+    private void HitCheck(Razer r)
+    {
+        var rCol = r.GetHit();
+
+        if (rCol != null)
+        {
+            var posi = rCol.transform.position;
+            //与ダメージ処理
+            rCol.GetComponent<Player>().Damege();
+            //AddDamage(rCol.gameObject);
+            //撃破パーティクル
+            //var p = Instantiate(particle);
+            //p.transform.position = posi;
+            //Destroy(p, 5f);
+        }
+    }
+
+    private void AddDamage(GameObject col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            //col.GetComponent<Player>().Damage();
+        }
+    }
+
+    /// <summary>
+    /// 停止
+    /// </summary>
+    public void Stop()
+    {
+        isStop = true;
+        foreach (var r in razerList)
+        {
+            r.Stop(isEnable);
+        }
+    }
 }
