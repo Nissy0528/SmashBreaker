23c60753ad8eafd0867e8657021ee706bbcbb8f2
ymstnao <me1a2do3@yahoo.co.jp>
2017-11-10 13:29:19 +0900

    Revert "Revert "ボスの平行移動追加""
    This reverts commit 95365b2ce20af94d0a801ae6ebf650da9c865ec9.

---
+++ Assets/Script/Utility/TranslationPositionSet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class TranslationPositionSet : MonoBehaviour
+{
+#if UNITY_EDITOR
+
+    private void OnDrawGizmos()
+    {
+        //マウスのクリックがあったら処理
+        if (Event.current == null || Event.current.type != EventType.mouseUp)
+        {
+            return;
+        }
+
+        //マウスの位置取得
+        Vector3 mousePosition = Event.current.mousePosition;
+
+        //シーン上の座標に変換
+        mousePosition.y = SceneView.currentDrawingSceneView.camera.pixelHeight - mousePosition.y;
+        mousePosition = SceneView.currentDrawingSceneView.camera.ScreenToWorldPoint(mousePosition);
+
+        Debug.Log("座標 : " + mousePosition.x.ToString("F2") + ", " + mousePosition.y.ToString("F2"));
+    }
+#endif
+}
