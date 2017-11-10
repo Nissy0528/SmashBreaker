545cdcbc565ee81ae89e14bbe3e54a18810098f3
ymstnao <me1a2do3@yahoo.co.jp>
2017-11-10 15:51:12 +0900

    矢印の追加

---
+++ Assets/Script/Actor/BossTargeter.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// bossへの矢印
+/// </summary>
+public class BossTargeter : MonoBehaviour
+{
+	/// <summary>
+	/// 目標
+	/// </summary>
+	[SerializeField]
+	private GameObject target;
+
+	/// <summary>
+	/// プレイヤー
+	/// </summary>
+	private GameObject player;
+
+	/// <summary>
+	/// 原点
+	/// </summary>
+	private Transform origin;
+
+	/// <summary>
+	/// 矢印
+	/// </summary>
+	[SerializeField]
+	private GameObject cursor;
+
+
+	// Use this for initialization
+	void Start()
+	{
+		if (target == null)
+		{
+			target = FindObjectOfType<Boss>().gameObject;
+		}
+
+		player = FindObjectOfType<Player>().gameObject;
+
+		var c = Instantiate<GameObject>(cursor);
+
+		c.transform.SetParent(transform);
+		c.transform.localPosition = Vector2.up;
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		cursor.SetActive(true);
+		if (target == null || player.GetComponent<Player>().IsDead())
+		{
+			cursor.SetActive(false);
+			return;
+		}
+		transform.position = player.transform.position;
+
+		Vector3 tp = target.transform.position;
+		Vector3 direction = (tp - transform.position).normalized;
+		transform.rotation = Quaternion.FromToRotation(Vector3.up, direction);
+	}
+}
